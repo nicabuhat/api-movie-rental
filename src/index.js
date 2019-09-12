e5922e03d5500ec9fe4bfee8701d3ba3ae7a6e3d
unknown <cabuhat.nicaela@gmail.com>
2019-09-12 08:34:12 +0800

    Added rentals model and route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 const express = require("express");
 const app = express();
 
@@ -29,6 +31,8 @@ app.get("/", (req, res) => {
 });
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 // PORT
 const port = process.env.PORT || 3000;
---
+++ src/routes/rentals.js
@@ -0,0 +1,65 @@
+const { Rental, validate } = require("../models/rental");
+const Fawn = require("fawn");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+//provide transaction capability to MongoDB
+Fawn.init(mongoose);
+
+// GET RENTALS
+router.get("/", async (req, res) => {
+  const rentals = await Rental.find().sort("-dateOut");
+  res.send(rentals);
+});
+
+// POST
+router.post("/", async (req, res) => {
+  //validate with Joi
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //retrieve customer by ID and send error if customer does not exist
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send("Invalid customer.");
+
+  //retrieve movies by ID and send error if movie does not exist
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send("Invalid movie.");
+
+  //check if movie is in stock else send an error
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not available");
+
+  //create rental object
+  let rental = new Rental({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate
+    }
+  });
+
+  //create transaction to perform clustered operation
+  try {
+    new Fawn.Task()
+      .save("rentals", rental)
+      .update(
+        "movies",
+        { _id: movie._id },
+        {
+          $inc: { numberInStock: -1 }
+        }
+      )
+      .run();
+  } catch (ex) {
+    res.status(500).send("Something failed");
+  }
+
+  res.send(rental);
+});
