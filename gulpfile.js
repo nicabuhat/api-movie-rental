3a74768288dac1bc84755b09b5b4ca1bc65c2a13
unknown <cabuhat.nicaela@gmail.com>
2019-09-19 22:50:57 +0800

    Updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,95 @@
-const { src, dest, watch } = require("gulp");
-const scss = require("gulp-sass");
-sass.compiler = require("node-sass");
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var nodemon = require("gulp-nodemon");
+var browserSync = require("browser-sync").create();
+// var uglify = require("gulp-uglify");
 
 /*
- * directories
+ * Directories here
  */
 let paths = {
   src: "./src/",
-  scss: "./src/sass/",
-  css: "./build/assets/styles/"
+  scss: "./src/public/sass/",
+  css: "./src/public/styles/"
   // data: "./src/"
 };
 
-//compile sass
-function sass() {
-  return src(paths.scss + "*.scss")
+// First, run all your tasks
+// gulp.task("default", ["nodemon", "sass", "js"], function() {
+gulp.task("default", ["nodemon", "sass"], function() {
+  // Then watch for changes
+  // gulp.watch("src/public/sass/*.scss", ["sass"]);
+  gulp.watch("src/public/sass/**/*.scss", sass);
+  gulp.watch("src/views/*.pug").on("change", browserSync.reload); //Manual Reloading
+
+  // JS changes need to tell browsersync that they're done
+  // gulp.watch("src/*/*.js", ["js-watch"]);
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+// gulp.task("js-watch", ["js"], function(done) {
+//   browserSync.reload();
+//   done();
+// });
+
+// Process JS files and return the stream.
+// gulp.task("js", function() {
+//   return (
+//     gulp
+//       .src("src/js/*.js")
+//       // .pipe(uglify())
+//       .pipe(gulp.dest("public/javascripts"))
+//   );
+// });
+
+// Compile SASS to CSS.
+// gulp.task("sass", function() {
+//   return gulp
+//     .src("src/public/sass/*.scss")
+//     .pipe(sass().on("error", sass.logError))
+//     .pipe(gulp.dest("src/public/styles"))
+//     .pipe(browserSync.stream());
+// });
+
+// Compile SASS to CSS.
+gulp.task("sass", function() {
+  return gulp
+    .src(paths.scss + "*.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(
-      scss({
+      sass({
         includePaths: [paths.scss],
         outputStyle: "compressed"
       })
     )
-    .pipe(dest(paths.css));
-}
+    .pipe(gulp.dest(paths.css))
+    .pipe(browserSync.stream());
+});
 
-//minify js
-// // Gulp task to minify JavaScript files
-// gulp.task("scripts", function() {
-//   return (
-//     gulp
-//       .src("./src/**/*.js")
-//       // Minify the file
-//       .pipe(uglify())
-//       // Output
-//       .pipe(gulp.dest("./assets/scripts"))
-//   );
+// Setup proxy for local server.
+// gulp.task("browser-sync", ["js", "sass"], function() {
+//   browserSync.init(null, {
+//     proxy: "http://localhost:3000",
+//     port: 7000
+//   });
 // });
+// Setup proxy for local server.
+gulp.task("browser-sync", ["sass"], function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:3000",
+    port: 7000
+  });
+});
 
-exports.default = function() {
-  watch("src/sass/**/*.scss", sass);
-};
+gulp.task("nodemon", ["browser-sync"], function(cb) {
+  var running = false;
+  return nodemon({ script: "src/index" }).on("start", function() {
+    if (!running) {
+      running = true;
+      cb();
+    }
+  });
+});
---
+++ sample.js
@@ -0,0 +1,4 @@
+// https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+//  https://api.themoviedb.org/3/movie/343611?api_key={api_key}
+// 10fa404088d797470afce6fe4b1fa635
+// https://www.themoviedb.org/settings/api
